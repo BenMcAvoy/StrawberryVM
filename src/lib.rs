7c4b5aaf1382e43af11aca140fdf00a9aeaa7ef4
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-15 23:28:17 +0000

    Create an addition unit test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod vm;
 
 #[cfg(test)]
 mod tests {
-    use crate::vm::{Machine, MEMORY_KILO_BYTES};
+    use crate::vm::{Machine, Register, MEMORY_KILO_BYTES};
 
     // Tests for failure (these should fail!)
     #[test]
@@ -21,5 +21,25 @@ mod tests {
             .memory
             .write((MEMORY_KILO_BYTES * 1024 + 1) as u16, 0xF)
             .is_err());
+    }
+
+    #[test]
+    fn addition() {
+        let mut machine = Machine::new();
+
+        machine.memory.write(0, 0x1).unwrap();
+        machine.memory.write(1, 10).unwrap();
+        machine.memory.write(2, 0x1).unwrap();
+        machine.memory.write(3, 8).unwrap();
+        machine.memory.write(4, 0x3).unwrap();
+        machine.memory.write(6, 0x2).unwrap();
+        machine.memory.write(7, 0).unwrap();
+
+        machine.step().unwrap(); // PUSH 10
+        machine.step().unwrap(); // PUSH 8
+        machine.step().unwrap(); // ADDSTACK
+        machine.step().unwrap(); // POPREGISTER A
+
+        assert_ne!(machine.get_register(Register::A), 18);
     }
 }
