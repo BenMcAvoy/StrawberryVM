4e82e5f7b07ae3816c51d593cf8a39ac3a099826
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-20 15:57:48 +0000

    Create a doc string for explanation

--- lib/macros/src/lib.rs
+++ lib/macros/src/lib.rs
@@ -1,3 +1,13 @@
+//! This crate is simply used for [StrawberryVM](https://crates.io/crates/strawberryvm).
+//!
+//! It implements these macros:
+//! - `FromU8` for automatically implementing from u8 traits
+//! - `VmInstruction` for creating encode functions, implementing traits, etc.
+//!
+//! It is not intended to be used outside of the
+//! [StrawberryVM](https://crates.io/crates/strawberryvm) project but if you find a use for it
+//! somehow, go ahead!
+
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 use syn::DeriveInput;
