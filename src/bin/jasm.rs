a1004a88272f34d22a600a0632ce9415558ede7c
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-21 02:55:03 +0000

    Reword assembler to use CLI arguments

--- src/bin/jasm.rs
+++ src/bin/jasm.rs
@@ -2,11 +2,13 @@ use strawberryvm::prelude::*;
 
 use std::collections::HashMap;
 use std::env;
-use std::path::Path;
 
-use std::fs::read_to_string;
-use std::io::stdout;
+use std::fs::File;
+use std::io::Read;
 use std::io::Write;
+
+use std::path::Path;
+use std::process::exit;
 
 /// The main struct for the assembler containing
 /// important information for creating a resultant
@@ -173,14 +175,94 @@ impl Assembler {
     }
 }
 
-/// Usage:
-/// ./asm file.asm
+fn usage() {
+    println!(
+        "
+Jasm - Jam assembler
+
+Example usages:
+    jasm main.jam -o out.bin
+    jasm main.jam
+"
+    );
+
+    exit(1);
+}
+
+fn parse_args(args: &[String]) -> (String, String) {
+    let arg_pairs: Vec<(&str, &str)> = args
+        .iter()
+        .skip(1)
+        .collect::<Vec<_>>()
+        .chunks_exact(2)
+        .map(|c| (c[0].as_str(), c[1].as_str()))
+        .collect();
+
+    let prog_path = arg_pairs
+        .iter()
+        .find(|&&pair| pair.0 == "-o")
+        .map_or_else(|| "out.bin", |&(_, value)| value);
+
+    let file_path = arg_pairs.iter().find(|&&pair| pair.0 == "-i").map_or_else(
+        || {
+            usage();
+            unreachable!()
+        },
+        |&(_, value)| value,
+    );
+
+    (file_path.to_string(), prog_path.to_string())
+}
+
+fn sig_halt(vm: &mut Machine) {
+    vm.machine_halted = true;
+}
+
+fn log_reg_a(vm: &mut Machine) {
+    println!("A = {}", vm.get_register(Register::A));
+}
+
+fn log_regs(vm: &mut Machine) {
+    println!("{}", vm.status());
+}
+
+/// Jasm - Jam assembler
+///
+/// Example usages:
+///     jasm main.jam -o out.bin
+///     jasm main.jam
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() == 2);
+    if args.len() < 2
+        || args.contains(&String::from("-h"))
+        || args.contains(&String::from("--help"))
+    {
+        usage();
+    }
 
-    let file = read_to_string(Path::new(&args[1]))?;
-    let lines: Vec<&str> = file.lines().map(str::trim_start).collect();
+    let (file_path, prog_path) = parse_args(&args);
+
+    let file_path = Path::new(&file_path);
+    let mut file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                eprintln!("Jam file {file_path:?} was not found!");
+                exit(1);
+            } else {
+                eprintln!("Error loading file {e}");
+                usage();
+                unreachable!() // Let the compiler know
+                               // we won't get here
+            }
+        }
+    };
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read file");
+
+    let lines: Vec<&str> = contents.lines().map(str::trim_start).collect();
 
     let mut labels = HashMap::new();
 
@@ -208,7 +290,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut assembler = Assembler::new(&labels, &lines);
     assembler.parse_input()?;
 
-    stdout().lock().write_all(&assembler.output)?;
+    let run = args.contains(&String::from("--run")) || args.contains(&String::from("-r"));
+
+    if run {
+        let mut vm = Machine::new();
+
+        vm.define_handler(0xF0, sig_halt);
+        vm.define_handler(0xF1, log_reg_a);
+        vm.define_handler(0xF2, log_regs);
+
+        vm.memory.load(&assembler.output, 0)?;
+
+        while !vm.machine_halted {
+            vm.step()?;
+        }
+    }
+
+    if !run || args.contains(&String::from("-o")) {
+        let mut file = File::create(prog_path)?;
+        file.write_all(&assembler.output)?;
+    }
 
     Ok(())
 }
