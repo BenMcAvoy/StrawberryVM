768000fa487a826e8b092a97590a94b59a3c7dc6
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-30 10:27:03 +0000

    Remove pointless representation signs

--- strawberry/src/register.rs
+++ strawberry/src/register.rs
@@ -1,15 +1,15 @@
 use strawberryvm_derive::{Display, FromStr, FromU8};
 
 /// Enum for registers, only really used
-/// to coordinate the register slice.
+/// to co-ordinate the register slice.
 #[derive(Debug, Clone, Copy, FromU8, Display, FromStr)]
 pub enum Register {
-    A = 0,  // General purpose
-    B = 1,  // General purpose
-    C = 2,  // General purpose
-    M = 3,  // Memory address register
-    SP = 4, // Stack pointer
-    PC = 5, // Program counter
-    BP = 6, // Base pointer
-    FL = 7, // Flags register
+    A,  // General purpose
+    B,  // General purpose
+    C,  // General purpose
+    M,  // Memory address register
+    SP, // Stack pointer
+    PC, // Program counter
+    BP, // Base pointer
+    FL, // Flags register
 }
